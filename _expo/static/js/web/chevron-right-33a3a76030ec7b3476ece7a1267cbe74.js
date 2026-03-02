5f2ceda299c77649e1c17b5b7a7d8a0b824f6c4a
FerdGitUCSD <fdanese@ucsd.edu>
2026-03-01 16:38:14 -0800

    Updates

---
+++ _expo/static/js/web/chevron-right-33a3a76030ec7b3476ece7a1267cbe74.js
@@ -0,0 +1,8 @@
+__d(function(g,r,i,a,m,e,d){"use strict";Object.defineProperty(e,'__esModule',{value:!0}),Object.defineProperty(e,"chevronRightSvg",{enumerable:!0,get:function(){return l}});const l=r(d[0]).svg`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`},7740,[5098]);
